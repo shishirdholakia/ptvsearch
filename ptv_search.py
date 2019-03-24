acd5c141bc5d15e7fbb0baaf0104a83ffde9c235
Shishir Dholakia <dholakia.shishir@berkeley.edu>
2019-03-24 04:31:10 -0700

    Updated ptv_search.py

--- ptv_search.py
+++ ptv_search.py
@@ -8,41 +8,37 @@ Created on Sun Mar 24 01:03:15 2019
 import lightkurve as lk
 import matplotlib.pyplot as plt
 import scipy
-pi = np.pi    
-def find_phase_OC(lc):
-    #Download light curve for each sector and stitch it into one light curve
-    starid =
-    frequency_list = []
-    
+import numpy as np
+
+pi = np.pi  
+  
+def find_phase_OC(lc,frequency_peaks):    
     #iterate over all the pulsation modes of the star find the OC
-    for freq in frequency_list:
+    for freq in frequency_peaks:
         def sinfunc(t,c, A):  return A * np.sin(2*pi*freq*(t+c)) + 1.0
-    
-    
-    
         #subdivide every light curve into 100 sections
         num_sections = 100
         time = list(divide_chunks(lc.time,num_sections))
         flux = list(divide_chunks(lc.flux,num_sections))
-        import numpy as np
+        guess_phase = lc.time[0]
+        guess_amp = np.std(lc.flux) * 2.**0.5
         periodlist = []
         mediantimelist = []
         #iterate over every window
         for index, interval in enumerate(time):
             #make the window into a TESSLightCurve object
-            lc_chunk = lk.LightCurve(interval, flux[index])
             guess = np.array([guess_phase,guess_amp])
             popt, pcov = scipy.optimize.curve_fit(sinfunc, interval, flux[index], p0=guess)
             periodlist.append(popt[0])
             mediantimelist.append(np.median(interval))
-        return(periodlist,mediantimelist)
+        return(86400*(np.array(periodlist)-np.mean(periodlist)),np.array(mediantimelist))
+        
 def divide_chunks(l, n): 
-  
-# looping till length l 
-for i in range(0, len(l), n):  
-    yield l[i:i + n]
+    # looping till length l 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n]
     
-def plot_LAT(path):
+def plot_LAT(path, mediantimelist, periodlist):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     
