65b3022b8d0c646877a0fa5cdde86e862741691c
Shashank Dholakia <shashankdholakia1@gmail.com>
2019-03-24 01:05:56 -0700

    file structure

---
+++ del_scu_search.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 00:51:15 2019
+
+@author: shashank
+"""
+
+class DelScuSearch:
+    
+    def find_delta_scuti:
+        pass
+    
+    def get_periodogram_quality():
+        pass
+    
+    def rank_by_quality():
+        pass
+    
+    
---
+++ ptv_search.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 01:03:15 2019
+
+@author: shashank
+"""
+class PTV_Search:
+    
+    def find_phase_OC():
+        pass
+    
+    def plot_LAT():
+        pass
+    
+    def find_period_OC():
+        pass
+    
+    def PTV_model():
+        pass
+    
+    def do_joint_fit():
+        pass
+
