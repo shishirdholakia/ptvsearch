86f5c26682b30626546dc5df34028e034753cb00
Shashank Dholakia <shashankdholakia1@gmail.com>
2019-07-15 18:44:17 -0700

    Created delScuModel class

--- del_scu_search.py
+++ del_scu_search.py
@@ -24,10 +24,11 @@ class Periodogram:
             return True
         elif len(self.peaks)==0:
             return False
-        
+
+    
     def get_discard_peaks(self,discard_threshold=0.01):
-        discard_peaks, dp_properties = find_peaks(self.power, prominence=[discard_threshold,self.threshold])
-        return discard_peaks
+        self.discard_peaks, properties = find_peaks(self.power, prominence=[discard_threshold,self.threshold])
+        return self.discard_peaks
         
         
     def get_periodogram_quality(self):
@@ -47,3 +48,14 @@ class Periodogram:
         
         
         return 
+    
+class delScuModel:
+    
+    def __init__(self):
+        pass
+    def simple_sin(t,freq, c, A):
+        return A * np.sin(2*pi*freq*(t+c)) + 1.0
+    def pm_sin(t,freq, c, A, pm_freq,pm_c,pm_A):
+        """Same as simple sine function, but uses a simple sine as a time
+        varying function to describe amplitude"""
+        return A * np.sin(2*pi*freq*(t + self.simple_sin(t,pm_freq,pm_c,pm_A))) + 1.0
